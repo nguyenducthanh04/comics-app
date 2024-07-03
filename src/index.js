831f791292e4c5e00c916f7923abf0217f73565c
nguyenducthanh04 <dducthanh04@gmail.com>
2024-07-03 10:37:24 +0700

    run build

--- src/index.js
+++ src/index.js
@@ -4,8 +4,6 @@ import "./index.css";
 import App from "./App";
 import GlobalStyles from "./components/GlobalStyles";
 
-// import reportWebVitals from './reportWebVitals';
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
